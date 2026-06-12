dffaeae074355a8b1750c476be12e9eb6055b5d9
Abhinav Dwivedi <143184201+Abhinavv-933@users.noreply.github.com>
2026-06-12 21:40:00 +0530

    fixing CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://pixnest-kappa.vercel.app/"
+      "https://pixnest-kappa.vercel.app"
     ],
     credentials: true
   })
