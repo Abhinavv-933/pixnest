2fe451b8048917ab1ca03b6a55b541982017498b
Abhinav Dwivedi <143184201+Abhinavv-933@users.noreply.github.com>
2026-06-12 20:52:08 +0530

    feat: update CORS configuration and enhance axios API setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,15 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://pixnest.abd.vercel.app"
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 // Routes
